8f1080e2691a1d23564a14e83e480f5b963406dc
stefan <think@hotmail.de>
2016-09-25 12:26:17 +0200

    cleaned up the first tidbits

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,4 @@ require "logstash/devutils/rspec/spec_helper"
 
 def total_seconds(days=0, hours=0, minutes=0, seconds=0)
   seconds + 60 * (minutes + 60 * (hours + 24 * days))
-end # def total_seconds
+end
