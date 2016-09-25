1a47ca58387d4bd2df481256096c4ea5dc6de8be
stefan <think@hotmail.de>
2016-09-25 17:39:02 +0200

    simplified

--- spec/filters/duration_spec.rb
+++ spec/filters/duration_spec.rb
@@ -1,9 +1,9 @@
 # encoding: utf-8
 require 'spec_helper'
-require "logstash/filters/duration"
+require 'logstash/filters/duration'
 
 describe LogStash::Filters::Duration do
-  describe "Parse duration in ISO format" do
+  describe 'Parse duration in ISO format' do
     let(:config) do <<-CONFIG
       filter {
         duration {
@@ -13,33 +13,32 @@ describe LogStash::Filters::Duration do
     CONFIG
     end
 
-    sample('iso' => "P1D") do
-      expect(subject.get("duration")).to eq(total_seconds(days: 1))
+    sample('iso' => 'P1D') do
+      expect(subject.get('duration')).to eq(total_seconds(days: 1))
     end
 
-    sample('iso' => "PT1H") do
-      expect(subject.get("duration")).to eq(total_seconds(hours: 1))
+    sample('iso' => 'PT1H') do
+      expect(subject.get('duration')).to eq(total_seconds(hours: 1))
     end
 
-    sample('iso' => "PT1M") do
-      expect(subject.get("duration")).to eq(total_seconds(minutes: 1))
+    sample('iso' => 'PT1M') do
+      expect(subject.get('duration')).to eq(total_seconds(minutes: 1))
     end
 
-    sample('iso' => "PT1S") do
-      expect(subject.get("duration")).to eq(total_seconds(seconds: 1))
+    sample('iso' => 'PT1S') do
+      expect(subject.get('duration')).to eq(total_seconds(seconds: 1))
     end
 
-# TODO: support for MS
-#    sample('iso' => "PT1S2MS") do
-#      expect(subject.get("duration")).to eq(total_seconds(seconds=1))
-#    end
-
-    sample('iso' => "P1DT2H3M4S") do
-      expect(subject.get("duration")).to eq(total_seconds(days: 1, hours: 2, minutes: 3, seconds: 4))
+    sample('iso' => 'PT1S2MS') do
+      expect(subject.get('duration')).to eq(total_seconds(seconds=1))
     end
 
-    sample('iso' => "-P1DT2H3M4S") do
-      expect(subject.get("duration")).to eq(-total_seconds(days: 1, hours: 2, minutes: 3, seconds: 4))
+    sample('iso' => 'P1DT2H3M4S') do
+      expect(subject.get('duration')).to eq(total_seconds(days: 1, hours: 2, minutes: 3, seconds: 4))
+    end
+
+    sample('iso' => '-P1DT2H3M4S') do
+      expect(subject.get('duration')).to eq(-total_seconds(days: 1, hours: 2, minutes: 3, seconds: 4))
     end
   end
 end
